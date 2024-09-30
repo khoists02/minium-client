a65502c43e040e5c8aedea5067ba849d2a4c7dee
Khoi LE <khoile93@Khois-MacBook-Pro.local>
2024-09-30 19:08:43 +0700

    config default

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import dynamicImport from "vite-plugin-dynamic-import";
+import dns from "dns";
 
-// https://vitejs.dev/config/
+dns.setDefaultResultOrder('verbatim')
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteTsConfigPaths(), dynamicImport()],
+  server: {
+    open: true,
+    host: "ui.ecommerce.local",
+    port: 3000,
+  },
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      output: {
+        chunkFileNames: () => "chunks/[name].[hash].js"
+      }
+    }
+  }
 })
