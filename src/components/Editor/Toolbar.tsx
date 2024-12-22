76766d792463270bb8276e131d14fd1cb43ea178
LÊ MINH KHÔI <khoi.kioto@gmail.com>
2024-12-22 11:08:44 +0700

    fix

--- src/components/Editor/Toolbar.tsx
+++ src/components/Editor/Toolbar.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 import { Tooltip, OverlayTrigger } from "react-bootstrap";
 import ToolbarButton from "./ToolbarButton";
-import { HighlightButton } from "./HighlightButton";
+// import { HighlightButton } from "./HighlightButton";
 
 interface ToolbarProps {
   wrapperClass?: string;
@@ -12,17 +12,20 @@ const Toolbar: FC<ToolbarProps> = ({
 }) => {
   const renderTooltip = (props: any) => (
     <Tooltip id="button-tooltip" {...props} className="toolbar-tooltip">
-      <ToolbarButton icon={<i className="fa fa-bold" />} format="bold" />
+      {/* <ToolbarButton icon={<i className="fa fa-bold" />} format="bold" /> */}
+      <ToolbarButton icon={<i className="fa fa-image " />} format="image" />
       <ToolbarButton className="ml-2" icon={<i className="fa fa-image " />} format="image" />
-      <HighlightButton className="ml-2" />
+      <ToolbarButton className="ml-2" icon={<i className="fa fa-image " />} format="image" />
+      <ToolbarButton className="ml-2" icon={<i className="fa fa-image " />} format="image" />
+      {/* <HighlightButton className="ml-2" /> */}
     </Tooltip>
   );
 
   return (
     <OverlayTrigger
       trigger={["click"]}
-      placement="top-start" // Position of the tooltip: top, right, bottom, left
-      delay={{ show: 250, hide: 400 }} // Delay in showing/hiding tooltip
+      placement="right" // Position of the tooltip: top, right, bottom, left
+      delay={{ show: 500, hide: 400 }} // Delay in showing/hiding tooltip
       overlay={renderTooltip} // Tooltip content
     >
       <i className={`fa fa-plus ${wrapperClass}`} />
