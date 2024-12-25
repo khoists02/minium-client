e9bec427af3c67a885ae32a58f55086c0dfb6d0b
LÊ MINH KHÔI <khoi.kioto@gmail.com>
2024-12-25 14:15:00 +0700

    add title

--- src/pages/admin/auth/RegisterContainer.tsx
+++ src/pages/admin/auth/RegisterContainer.tsx
@@ -1,3 +1,13 @@
+/*
+ * Mimium Pty. Ltd. ("LKG") CONFIDENTIAL
+ * Copyright (c) 2022 Mimium project Pty. Ltd. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of LKG. The intellectual and technical concepts contained
+ * herein are proprietary to LKG and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material is strictly forbidden unless prior written permission is obtained
+ * from LKG.  Access to the source code contained herein is hereby forbidden to anyone except current LKG employees, managers or contractors who have executed
+ * Confidentiality and Non-disclosure agreements explicitly covering such access.
+ */
 import React from "react";
 import { RegistrationForm } from "./RegisterForm";
 
@@ -8,7 +18,7 @@ const RegistrationContainer: React.FC = () => {
             <div className="container mt-5">
                 <div className="row justify-content-center">
                     <div className="col-md-6">
-                        <RegistrationForm onSubmit={() => {}} />
+                        <RegistrationForm onSubmit={() => { }} />
                     </div>
                 </div>
             </div>
--- src/pages/admin/auth/RegisterForm.tsx
+++ src/pages/admin/auth/RegisterForm.tsx
@@ -1,3 +1,13 @@
+/*
+ * Mimium Pty. Ltd. ("LKG") CONFIDENTIAL
+ * Copyright (c) 2022 Mimium project Pty. Ltd. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of LKG. The intellectual and technical concepts contained
+ * herein are proprietary to LKG and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material is strictly forbidden unless prior written permission is obtained
+ * from LKG.  Access to the source code contained herein is hereby forbidden to anyone except current LKG employees, managers or contractors who have executed
+ * Confidentiality and Non-disclosure agreements explicitly covering such access.
+ */
 import React, { useState, FC } from "react";
 
 export interface ISubmitForm {
